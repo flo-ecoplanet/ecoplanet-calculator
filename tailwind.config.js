d62c1a1559ba408ce28ac55cd475da7c7ef82859
Florian Siebels <florian.siebels@ecoplanet.tech>
2025-07-01 14:01:04 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Geistvf: ['Geistvf', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
