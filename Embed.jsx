0deaca2bdb94ccd733851529bbb180d4c9b2ea06
flo-ecoplanet <florian.siebels@ecoplanet.tech>
2025-07-01 15:12:28 +0200

    Create Embed.jsx

---
+++ Embed.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+
+export default function Embed() {
+  return (
+    <div className="min-h-screen bg-white overflow-auto">
+      <App />
+    </div>
+  );
+}
