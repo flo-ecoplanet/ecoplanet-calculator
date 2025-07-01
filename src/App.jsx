5a8b2ef0623bf894fd00c16dea6b476133dfb353
flo-ecoplanet <florian.siebels@ecoplanet.tech>
2025-07-01 14:52:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
-<inserted from canvas>
+import React from "react";
+import EcoplanetCalculator from "./EcoplanetCalculator";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#f5f5f5]">
+      <EcoplanetCalculator />
+    </div>
+  );
+}
