bc6ca8922dc4f9912fae58c5df783021d4f8d908
Florian Siebels <florian.siebels@ecoplanet.tech>
2025-07-02 10:29:12 +0200

    💄 Use Geistvf font and add icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
@@ -8,4 +8,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
